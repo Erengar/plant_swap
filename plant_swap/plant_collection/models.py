76c761543b726b27e62485a858d450d1505355be
Adam Kubik <kubik.adam445@gmail.com>
2023-09-21 17:41:14 +0200

    added tags and history to plant model

---
+++ plant_swap/plant_collection/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+from tagging.managers import ModelTagManager
+from django.conf import settings
+from simple_history.models import HistoricalRecords
+# Create your models here.
+
+class Species(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Plant(models.Model):
+    nick_name = models.CharField(
+        max_length=64,
+        validators=[MinLengthValidator(3)])
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, null=True, blank=True)
+    species = models.ForeignKey(Species, on_delete=models.PROTECT, related_name='plants')
+    tags = ModelTagManager()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    history = HistoricalRecords(excluded_fields=['tags', 'species', 'nick_name', 'updated'])
+
+    def __str__(self):
+        return self.nick_name
