0ff9782bc15afd015a6bc380642c806ad47a7b35
Adam Kubik <kubik.adam445@gmail.com>
2023-10-01 02:11:38 +0200

    files are removed upon their model instance being removed, some small html changes

--- plant_swap/plant_collection/models.py
+++ plant_swap/plant_collection/models.py
@@ -4,6 +4,13 @@ from django.conf import settings
 from simple_history.models import HistoricalRecords
 from django.utils.text import slugify
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_delete
+
+    
+
 # Create your models here.
 
 class Species(models.Model):
@@ -50,3 +57,28 @@ class Image(models.Model):
 
     def __str__(self):
         return str(self.plant.nick_name) + ' ' + str(self.pk)
+    
+
+@receiver(post_delete, sender=Image)
+def post_save_image(sender, instance, *args, **kwargs):
+    """ Clean Old Image file """
+    try:
+        instance.image.delete(save=False)
+    except:
+        pass
+
+@receiver(pre_save, sender=Image)
+def pre_save_image(sender, instance, *args, **kwargs):
+    """ instance old image file will delete from os """
+    try:
+        old_img = instance.__class__.objects.get(id=instance.id).image.path
+        try:
+            new_img = instance.image.path
+        except:
+            new_img = None
+        if new_img != old_img:
+            import os
+            if os.path.exists(old_img):
+                os.remove(old_img)
+    except:
+        pass
