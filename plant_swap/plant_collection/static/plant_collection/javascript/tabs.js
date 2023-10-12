88c807e9b5c4542d5d133b4c494b7bc835f36c8b
Adam Kubik <kubik.adam445@gmail.com>
2023-10-12 03:19:51 +0200

    started changing color design

--- plant_swap/plant_collection/static/plant_collection/javascript/tabs.js
+++ plant_swap/plant_collection/static/plant_collection/javascript/tabs.js
@@ -2,12 +2,12 @@ var url = window.location.href;
 
 
 if (url.includes("my-collection")) {
-    $('.navbar-item[href="/my-collection/"]').addClass("is-active");
+    $('.navbar-item[href="/my-collection/"]').addClass("navbar-active");
 } else if (url === 'http://127.0.0.1:8000/') {
-    $('.navbar-item[href="/"]').addClass("is-active");
+    $('.navbar-item[href="/"]').addClass("navbar-active");
 } else if (url.includes('species')) {
     var myspecie = url.split("/")[4];
-    $('a[href="/species/' + myspecie + '/"]').addClass("is-active");
+    $('a[href="/species/' + myspecie + '/"]').addClass("sage-green");
 } else if (url.includes('accounts/trades')) {
-    $('a[href="/accounts/trades/"]').addClass("is-active");
+    $('a[href="/accounts/trades/"]').addClass("navbar-active");
 }
