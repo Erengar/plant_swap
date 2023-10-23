b6f7fdf6aa0b5d7855291024ba40453c469d36db
Adam Kubik <kubik.adam445@gmail.com>
2023-10-23 02:26:06 +0200

    fixes for tablet

--- plant_swap/plant_collection/static/plant_collection/javascript/delete_modal.js
+++ plant_swap/plant_collection/static/plant_collection/javascript/delete_modal.js
@@ -1,6 +1,9 @@
 $('.modal_open').on('click', function() {
+    if (!$('.modal_open').prop('disabled')) {
     $('.modal').addClass('is-active')
+    }
 });
+
 $('.modal-close').on('click', function() {
     $('.modal').removeClass('is-active')
 })
