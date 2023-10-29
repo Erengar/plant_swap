fbf0a11102f2f3c19c98e28aa657e531452c82ac
Adam Kubik <kubik.adam445@gmail.com>
2023-10-29 17:23:02 +0100

    trying some media stuff

--- plant_swap/settings.py
+++ plant_swap/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 
 #Social logins
