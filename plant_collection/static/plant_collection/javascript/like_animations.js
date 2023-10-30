e9e9aee7a480ff19569a00cc34157e2dad656882
Adam Kubik <kubik.adam445@gmail.com>
2023-10-30 22:33:03 +0100

    added github social login

--- plant_collection/static/plant_collection/javascript/like_animations.js
+++ plant_collection/static/plant_collection/javascript/like_animations.js
@@ -1,9 +1,10 @@
 $('.fa-heart.has-text-danger').on('click', function() {
-    if($(this).hasClass('fa-regular')){
+    if($(this).hasClass('fa-regular') && $('a[href="/accounts/logout/"]').length){
     $(this).addClass('animation');
     setTimeout(() => {
         $(this).removeClass('animation');
     }, 300)
     };
+    console.log($('a[href="accounts/logout"]'))
     
 });
