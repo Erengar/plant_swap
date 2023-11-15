d030c84aabee56c8fefcebd3e5e744e3b4e14abd
Adam Kubik <kubik.adam445@gmail.com>
2023-11-15 01:11:32 +0100

    var/media-save/media

--- plant_swap/settings.py
+++ plant_swap/settings.py
@@ -159,6 +159,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+MEDIA_ROOT = 'var/media-save/media'
 
 #Social logins
 
