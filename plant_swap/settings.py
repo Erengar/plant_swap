1c6af75c6ba7fcc67d8e40900ef8f9afd93e90e9
Adam Kubik <kubik.adam445@gmail.com>
2023-11-15 01:03:19 +0100

    reversing

--- plant_swap/settings.py
+++ plant_swap/settings.py
@@ -159,7 +159,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_ROOT = 'var/media-save/media'
 
 #Social logins
 
